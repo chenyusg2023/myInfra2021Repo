0541fe4bd071491709b238fce924148a5d37596a
chenyusg2023 <124852850+chenyusg2023@users.noreply.github.com>
2024-09-22 22:23:41 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "demo-checkpoint-s3"
     key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    region = "ap-south-1"
   }
 }
